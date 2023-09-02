6eb40bc7acc0a3f113c42a48bd2171e719d0e6ae
CASTILLOAXEL <140232308+CASTILLOAXEL@users.noreply.github.com>
2023-09-01 20:22:24 -0600

    Add files via upload

--- src/main/java/PkgPizza/Pizza.java
+++ src/main/java/PkgPizza/Pizza.java
@@ -1,2 +1,27 @@
-package PkgPizza;public class Pizza {
+package PkgPizza;
+
+public class Pizza {
+    private String name;
+    private double precioBase;
+    private Topping[] toppings;
+
+    //Constructor
+ public Pizza(String name, double precioBase, Topping[] toppings) {
+        this.name = name;
+        this.precioBase = precioBase;
+        this.toppings = toppings;
+    }
+//getters
+
+    public String getName() {
+        return name;
+    }
+    public double getPrecioBase() {
+        return precioBase;
+    }
+
+    public Topping[] getToppings() {
+        return toppings;
+    }
+
 }
--- src/main/java/PkgPizza/Topping.java
+++ src/main/java/PkgPizza/Topping.java
@@ -1,2 +1,27 @@
-package PkgPizza;public class Topping {
+package PkgPizza;
+
+public class Topping {
+
+            private String nombre;
+            private double price;
+
+            public Topping(String nombre, double price) {
+                this.nombre = nombre;
+                this.price = price;
+            }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+
+
+        public double getPrice() {
+            return price;
+        }
+
+        @Override
+        public String toString()
+        { return "Topping{" + "nombre='" + nombre + '\'' + ", price=" + price + '}';
+        }
 }
